f656f71e22516bcd9d45e56d6d36942ff1df626b
juancarlos2v <juanc.vilcherrez@gmail.com>
2024-12-08 17:17:47 -0300

    ✨ Menu

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package com.alura.literalura;
 
+import com.alura.literalura.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,19 +24,56 @@ public class LiteraluraApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("====Alura Challenge===");
-        Scanner scan = new Scanner(System.in);
+        //var consumoAPI = new ConsumoAPI();
+        //var json= consumoAPI.getData("http://gutendex.com/books/?page=1");
+        System.out.println("====LiterAlura====");
+        Scanner scanner = new Scanner(System.in);
 
-        HttpClient client = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://gutendex.com/books"))
-                .build();
-        System.out.println("Enviando solicitud...");
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        while (true) {
+            System.out.println("===== Menú de opciones =====");
+            System.out.println("1: Consultar libros");
+            System.out.println("2: Buscar libro por título");
+            System.out.println("3: Consultar autores");
+            System.out.println("4: Consultar autores vivos por año");
+            System.out.print("Seleccione una opción: ");
 
-        System.out.println(response.body());
+            if (scanner.hasNextInt()) {
+                int opcion = scanner.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        // Acción para consultar libros
+                        System.out.println("Consultando libros...");
+                        break;
+                    case 2:
+                        // Acción para buscar libro por título
+                        System.out.println("Buscando libro por título...");
+                        break;
+                    case 3:
+                        // Acción para consultar autores
+                        System.out.println("Consultando autores...");
+                        break;
+                    case 4:
+                        // Acción para consultar autores vivos por año
+                        System.out.println("Consultando autores vivos por año...");
+                        break;
+                    default:
+                        // Opción no válida, continuar el bucle
+                        System.out.println("Opción no válida. Por favor, ingrese una opción válida.");
+                        continue;
+                }
+                // Si llegamos aquí, se seleccionó una opción válida, entonces salimos del bucle
+                break;
+            } else {
+
+                System.out.println("Entrada no válida. Por favor, ingrese un número.");
+                scanner.next();
+            }
+        }
+
+
+        //System.out.println(json);
 
 
         //opciones de insercion y consulta de libros
